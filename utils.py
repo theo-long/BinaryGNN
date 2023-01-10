f956a28cdb47b2e94a57c5667562e5a15d0a6978
Theo Long <theodorelong98@gmail.com>
2023-01-10 13:45:08 -0500

    update utils

--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ from spektral.layers import GCNConv
 from spektral.data import SingleLoader
 
 import numpy as np
+import pandas as pd
+from tqdm import trange
+import gc
 
 
 def prepare_dataset(dataset):
@@ -41,3 +44,65 @@ def generate_data_loaders(dataset):
     loader_te = SingleLoader(dataset, sample_weights=weights_te)
 
     return loader_tr, loader_va, loader_te
+
+
+def run_experiment(
+    experiment_name,
+    num_runs,
+    epochs,
+    dataset,
+    model_factory,
+    optimizer,
+    loss_function,
+    callbacks,
+    reduce_memory_overhead=True,
+    verbose=0,
+):
+    loader_tr, loader_va, loader_te = generate_data_loaders(dataset)
+
+    history_dataframes = []
+    eval_data = []
+    for run in trange(num_runs):
+        model = model_factory()
+
+        model.compile(
+            optimizer=optimizer, loss=loss_function, weighted_metrics=["accuracy"]
+        )
+
+        history = model.fit(
+            loader_tr.load(),
+            steps_per_epoch=loader_tr.steps_per_epoch,
+            validation_data=loader_va.load(),
+            validation_steps=loader_va.steps_per_epoch,
+            epochs=epochs,
+            callbacks=callbacks,
+            verbose=verbose,
+        )
+        eval_results = model.evaluate(
+            loader_te.load(), steps=loader_te.steps_per_epoch, verbose=verbose
+        )
+
+        # Do this to reduce memory overhead since we are using dense tensors
+        if reduce_memory_overhead:
+            del model
+            gc.collect()
+
+        history_df = pd.DataFrame(history.history)
+        history_df["run"] = run
+        history_dataframes.append(history_df)
+
+        eval_data.append(eval_results)
+
+    history_df = pd.concat(history_dataframes)
+    eval_df = pd.DataFrame(eval_data).reset_index()
+    eval_df.columns = ["run", "test_loss", "test_accuracy"]
+
+    history_df.to_csv(f"./data/{experiment_name}_{dataset.name}_history.csv")
+    eval_df.to_csv(f"./data/{experiment_name}_{dataset.name}_eval.csv")
+
+    mean_train_acc = history_df.groupby("run")["accuracy"].max().mean()
+    mean_val_acc = history_df.groupby("run")["val_accuracy"].max().mean()
+    mean_test_acc = eval_df["test_accuracy"].mean()
+    print(
+        f"{num_runs} runs completed: {mean_train_acc:.4f} mean train acc, {mean_val_acc:.4f} mean val acc, {mean_test_acc:.4f} mean test acc"
+    )
