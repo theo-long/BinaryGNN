8a62275d64401056f355b2202826c44b9cea5b27
Theo Long <theodorelong98@gmail.com>
2023-01-10 17:43:30 -0500

    add analysis utils and model checkpointing

--- utils.py
+++ utils.py
@@ -7,8 +7,11 @@ from spektral.data import SingleLoader
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from tqdm import trange
 import gc
+import glob
+import re
 
 
 def prepare_dataset(dataset):
@@ -62,8 +65,16 @@ def run_experiment(
 
     history_dataframes = []
     eval_data = []
+
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        "model.tf",
+        monitor="val_accuracy",
+        save_best_only=True,
+        save_weights_only=True,
+    )
+    callbacks.append(checkpoint_callback)
     for run in trange(num_runs):
-        model = model_factory()
+        model: tf.keras.Model = model_factory()
 
         model.compile(
             optimizer=optimizer, loss=loss_function, weighted_metrics=["accuracy"]
@@ -78,6 +89,8 @@ def run_experiment(
             callbacks=callbacks,
             verbose=verbose,
         )
+        # Restore best weights
+        model.load_weights("model.tf")
         eval_results = model.evaluate(
             loader_te.load(), steps=loader_te.steps_per_epoch, verbose=verbose
         )
@@ -106,3 +119,65 @@ def run_experiment(
     print(
         f"{num_runs} runs completed: {mean_train_acc:.4f} mean train acc, {mean_val_acc:.4f} mean val acc, {mean_test_acc:.4f} mean test acc"
     )
+
+
+def plot_training_curves(
+    history_df,
+    hyperparameter_name,
+    error_bars=True,
+    acc_plot_kwargs=None,
+    loss_plot_kwargs=None,
+):
+    if not acc_plot_kwargs:
+        acc_plot_kwargs = {}
+    if not loss_plot_kwargs:
+        loss_plot_kwargs = {}
+
+    mean_df = history_df.groupby(["epoch", hyperparameter_name])[
+        ["loss", "accuracy", "val_loss", "val_accuracy"]
+    ].mean()
+    std_df = history_df.groupby(["epoch", hyperparameter_name])[
+        ["loss", "accuracy", "val_loss", "val_accuracy"]
+    ].std()
+
+    try:
+        hyper_values = history_df[hyperparameter_name].sort_values().unique()
+    except Exception:
+        hyper_values = history_df[hyperparameter_name].unique()
+
+    plots = []
+    for metric in ["accuracy", "loss"]:
+        for prefix in ["", "val_"]:
+            plot_kwargs = acc_plot_kwargs if metric == "accuracy" else loss_plot_kwargs
+            ax = mean_df[prefix + metric].unstack().plot(**plot_kwargs)
+            if error_bars:
+                lower = (mean_df - std_df)[prefix + metric].unstack()
+                upper = (mean_df + std_df)[prefix + metric].unstack()
+                for hyper_value in hyper_values:
+                    plt.fill_between(
+                        lower.index, lower[hyper_value], upper[hyper_value], alpha=0.4
+                    )
+
+            ax.set_title(f"{prefix.strip('_').title()} {metric.capitalize()}")
+            plots.append(ax)
+
+    return plots
+
+
+def generate_dataframes_from_files(file_string, param_column=None):
+    eval_dfs = []
+    history_dfs = []
+    for f in glob.glob(f"{file_string}*eval*"):
+        df = pd.read_csv(f)
+        if param_column:
+            df[param_column] = float(re.match(".*=(.*)_c", f).groups()[0])
+        eval_dfs.append(df)
+    for f in glob.glob(f"{file_string}*history*"):
+        df = pd.read_csv(f)
+        if param_column:
+            df[param_column] = float(re.match(".*=(.*)_c", f).groups()[0])
+        history_dfs.append(df)
+    eval_df = pd.concat(eval_dfs)
+    history_df = pd.concat(history_dfs)
+    history_df = history_df.rename(columns={"Unnamed: 0": "epoch"})
+    return eval_df, history_df
